bf5d03ccd8fccee9c17c6a6c93d9eab8d468aecf
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-05-25 20:36:08 -0300

    add

--- JavaScript/modules/menu_mobile.js
+++ JavaScript/modules/menu_mobile.js
@@ -1,14 +1,26 @@
-const menuMobile = document.querySelector('[data-menu-mobile]')
+const menuMobile = document.querySelector('[data-btn-mobile]')
 
 
-function toggleMenu(){
+function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault()
+    const navMobile = document.querySelector('[data-nav-menu]');
+    navMobile.classList.toggle('active');
+    const active = navMobile.classList.contains('active');
+    event.currentTarget.setAttribute('aria-axpanded', active);
+    if(active){
+        event.currentTarget.setAttribute('aria-label','fechar menu');
+    }else{
+        event.currentTarget.setAttribute('aria-label','abrir menu')
+    }
     
 }
 
 
 function setListeners(){
-    window.addEventListener('click', toggleMenu )
+
+    menuMobile.addEventListener('click', toggleMenu);
+    menuMobile.addEventListener('touchstart',toggleMenu);
+    
 }
 
 
