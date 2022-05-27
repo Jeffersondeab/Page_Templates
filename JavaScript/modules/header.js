7a70cbfdef9537148961fbc6bd938feb4eba3cc1
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-05-27 00:53:34 -0300

    add

--- JavaScript/modules/header.js
+++ JavaScript/modules/header.js
@@ -3,9 +3,9 @@ const header = document.querySelector('[data-header]')
 
 function onWindowScroll(){
     if(window.scrollY > 20){
-        header.style.backgroundColor = 'grey'
+        header.style.backgroundColor = '#478dae'
     }else{
-        header.style.backgroundColor = 'transparent'
+        header.style.backgroundColor = 'white'
     }
 }
 
