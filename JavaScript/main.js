c8b6b71e68f1151a1597b68fdc5dff06da6911cf
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-06-03 19:02:30 -0300

    add

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,7 @@
 import header from './modules/header.js'
 import menu_mobile  from './modules/menu_mobile.js'
+import carousel from './modules/carousel.js'
 
 header.init()
 menu_mobile.init()
+carousel.init()
