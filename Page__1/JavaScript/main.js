3713e3e1e59f62d8dc73a4509e5b96905342dab7
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-07-13 00:43:11 -0300

    add

--- Page__1/JavaScript/main.js
+++ Page__1/JavaScript/main.js
@@ -1,7 +1,6 @@
 import header from './modules/header.js'
 import menu_mobile  from './modules/menu_mobile.js'
-import carousel from './modules/carousel.js'
+
 
 header.init()
 menu_mobile.init()
-carousel.init()
--- Page__1/JavaScript/modules/header.js
+++ Page__1/JavaScript/modules/header.js
@@ -4,6 +4,7 @@ const header = document.querySelector('[data-header]')
 function onWindowScroll(){
     if(window.scrollY > 20){
         header.style.backgroundColor = '#478dae'
+        
     }else{
         header.style.backgroundColor = 'white'
     }
